46ce03d68344abfcaa2ee5298786d37a918a623e
AldyColares <aldycolares@gmail.com>
2016-10-17 20:52:59 -0200

    Já é alguma coisa

--- glwidget.cpp
+++ glwidget.cpp
@@ -1,6 +1,29 @@
 #include "glwidget.h"
 
-GLWidget::GLWidget()
+GLWidget::GLWidget(QWidget *parent) :
+    QGLWidget(parent)
+{
+
+}
+
+void GLWidget::initializeGL()
+{
+    glClearColor(0.0f,0.0f,0.0f,1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_COLOR_MATERIAL);
+
+
+}
+
+void GLWidget::paintGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+}
+
+void GLWidget::resize(int w, int h)
 {
 
 }
--- glwidget.h
+++ glwidget.h
@@ -1,11 +1,17 @@
 #ifndef GLWIDGET_H
 #define GLWIDGET_H
 
+#include <QGLWidget>
 
 class GLWidget : public QGLWidget
 {
+    Q_OBJECT
 public:
-    GLWidget();
+    explicit GLWidget(QWidget *parent = 0);
+
+    void initializeGL();
+    void paintGL();
+    void resize(int w, int h);
 };
 
 #endif // GLWIDGET_H
